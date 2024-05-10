89229a9c6c72a8fec43216d197eaa0eaaf4f95b7
Artem Taraskin <127009838+misterT1A@users.noreply.github.com>
2024-05-10 12:11:45 +0300

    feat: RSS-ECOMM-2_19 Implementation of routing (#83)
    * feat: added app class, router basic structure, pages file with pages, create typesfile fot router
    
    * fix: router class, app class

---
+++ src/app/router/router.ts
@@ -0,0 +1,53 @@
+export default class Router {
+  protected routes: IRoute[];
+
+  constructor(routes: IRoute[]) {
+    this.routes = routes;
+
+    window.addEventListener('popstate', this.changeBrowser.bind(this));
+    document.addEventListener('DOMContentLoaded', this.navigateToLastPoint.bind(this));
+  }
+
+  public navigate(url: string, popstate = false) {
+    // to do / for products path
+    // const request = this.parseUrl(url);
+    // const pathForFind = request.resource === '' ? request.path : `${request.path}/${id}`;
+
+    const route = this.routes.find((routeItem) => routeItem.path === url);
+    if (!route) {
+      console.log('no matches');
+      // to do / for 404 page
+      return;
+    }
+    route.callBack();
+    if (!popstate) {
+      window.history.pushState(null, '', `/${url}`);
+    }
+  }
+
+  public navigateToLastPoint() {
+    const path = this.getCurrentPath();
+    this.navigate(path);
+  }
+
+  // to do / for products path
+  // private parseUrl(url: string) {
+  //   const result = {
+  //     path: '',
+  //   };
+
+  //   const path = url.split('/');
+  //   [result.path = '', result.resource = ''] = path;
+
+  //   return result;
+  // }
+
+  private changeBrowser(): void {
+    const path = this.getCurrentPath();
+    this.navigate(path, true);
+  }
+
+  private getCurrentPath() {
+    return window.location.pathname.slice(1);
+  }
+}
