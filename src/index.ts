cf8e4d63546c59a9be840382cea74b356081a972
tatsianask108 <tt.laurie@gmail.com>
2024-05-11 22:23:42 +0300

    refactor: remove comments from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ import './index.html';
 import './style.scss';
 
 import App from './app/app';
-// import AuthService from './app/services/auth-service';
 
 const app = new App();
 app.showContent(document.body);
-
-// const apiRoot = AuthService.getRoot();
-// apiRoot.get().execute();
