f3cf57caf58304440440474220337672f4c9a206
Tatsiana Skavarodka <121252461+tatsianask108@users.noreply.github.com>
2024-05-15 19:16:19 +0300

    feat: RSS-ECOMM-2_08 Implement navigation to Registration Page #102
    Added a link on the login page that allows users to navigate to the registration page.

--- src/app/components/login-form/login-view.ts
+++ src/app/components/login-form/login-view.ts
@@ -1,13 +1,32 @@
+import Pages from '@src/app/router/pages';
+import type Router from '@src/app/router/router';
 import BaseComponent from '@utils/base-component';
-import { h2, p } from '@utils/elements';
+import { div, h2, p, span } from '@utils/elements';
 
 import style from './_login-form.scss';
 
 export default class LoginView extends BaseComponent<HTMLFormElement> {
-  public constructor() {
+  public linkContainer: BaseComponent;
+
+  public constructor(router: Router) {
     super({ tag: 'form', noValidate: true, className: style['login-form'], action: '#' });
-    this.appendChildren([h2([style['form-title']], 'Login'), p([style['form-text']], 'Please, enter your email and password')]);
+    this.linkContainer = div(
+      [style['link-container']],
+      span([style.plug], "Don't have an account?"),
+      new BaseComponent<HTMLAnchorElement>({
+        tag: 'a',
+        textContent: 'Register',
+        onclick: () => {
+          router.navigate(Pages.REG);
+        },
+      })
+    );
+    this.appendChildren([
+      h2([style['form-title']], 'Login'),
+      p([style['form-text']], 'Please, enter your email and password'),
+    ]);
     this.renderFormContent();
+    this.append(this.linkContainer);
   }
 
   protected renderFormContent(): void {
@@ -41,14 +60,7 @@ export default class LoginView extends BaseComponent<HTMLFormElement> {
 
     this.append(this.createInputContainer('Password', passwordInput, passwordEye));
 
-    this.append(
-      new BaseComponent<HTMLButtonElement>({
-        tag: 'button',
-        className: style.button,
-        type: 'submit',
-        textContent: 'Login',
-      })
-    );
+    this.append(this.createSubmitButton());
   }
 
   protected createInputContainer(title: string, ...elements: BaseComponent[]): BaseComponent<HTMLLabelElement> {
@@ -64,5 +76,14 @@ export default class LoginView extends BaseComponent<HTMLFormElement> {
         className: style['error-container'],
       })
     );
+  }
+
+  protected createSubmitButton() {
+    return new BaseComponent<HTMLButtonElement>({
+      tag: 'button',
+      className: style.button,
+      type: 'submit',
+      textContent: 'Login',
+    });
   }
 }
