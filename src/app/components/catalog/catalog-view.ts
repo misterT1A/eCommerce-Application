33f5ddab7f19f9f723fdde7defe1b86af799372c
tatsianask108 <tt.laurie@gmail.com>
2024-05-23 14:21:42 +0300

    feat: add filter component

--- src/app/components/catalog/catalog-view.ts
+++ src/app/components/catalog/catalog-view.ts
@@ -2,17 +2,21 @@ import type Router from '@src/app/router/router';
 import BaseComponent from '@utils/base-component';
 
 import styles from './_style.scss';
+import FilterBlock from './filters-block';
 
 export default class CatalogView extends BaseComponent {
   protected router: Router;
+
+  protected filterBlock: FilterBlock;
 
   constructor(router: Router) {
     super({ tag: 'section', className: styles.wrapper });
     this.router = router;
 
-    // this.filterBlock = new FilterBlock()
+    this.filterBlock = new FilterBlock();
     // this.productCardsBlock = new ProductCardsBlock()
 
+    this.append(this.filterBlock);
     // this.appendChildren([this.filterBlock, this.productCardsBlock]);
   }
 }
