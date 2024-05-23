b9f19955d67459c382d44e2eb5898ccb43b20e06
tatsianask108 <tt.laurie@gmail.com>
2024-05-23 20:26:44 +0300

    feat: get filtered products from API

--- src/app/components/catalog/filter-block.ts
+++ src/app/components/catalog/filter-block.ts
@@ -1,4 +1,5 @@
 import Toggler from '@components/form-ui-elements/formToggler';
+import AuthService from '@services/auth-service';
 import BaseComponent from '@utils/base-component';
 
 import styles from './_filters.scss';
@@ -16,5 +17,64 @@ export default class FilterBlock extends BaseComponent {
     this.veganFilter = new Toggler('Vegan');
     this.forKidsFilter = new Toggler('For kids');
     this.appendChildren([this.salesFilter, this.veganFilter, this.forKidsFilter]);
+    this.initListeners();
+  }
+
+  public initListeners() {
+    this.salesFilter.addListener('change', () => {
+      console.clear();
+      if (this.salesFilter.getValue()) {
+        this.getDiscountedProducts();
+      }
+    });
+    this.veganFilter.addListener('change', () => {
+      console.clear();
+      if (this.veganFilter.getValue()) {
+        this.getProductsByAttribute('Vegan');
+      }
+    });
+    this.forKidsFilter.addListener('change', () => {
+      console.clear();
+      if (this.forKidsFilter.getValue()) {
+        this.getProductsByAttribute('ForKids');
+      }
+    });
+  }
+
+  public getDiscountedProducts() {
+    AuthService.getRoot()
+      .productProjections()
+      .search()
+      .get({
+        queryArgs: {
+          priceCurrency: 'EUR',
+          filter: 'variants.scopedPriceDiscounted:true',
+        },
+      })
+      .execute()
+      .then((response) => {
+        console.log(response.body.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  public getProductsByAttribute(attr: string) {
+    AuthService.getRoot()
+      .productProjections()
+      .search()
+      .get({
+        queryArgs: {
+          filter: `variants.attributes.${attr}:true`,
+        },
+      })
+      .execute()
+      .then((response) => {
+        console.log(response.body.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
