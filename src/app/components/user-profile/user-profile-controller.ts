7366006db0fa0fa4c6a784c281cba9e0b9d53f74
Alv0425 <vilejshikova@gmail.com>
2024-05-25 01:40:08 +0300

    feat: update user profile controller

--- src/app/components/user-profile/user-profile-controller.ts
+++ src/app/components/user-profile/user-profile-controller.ts
@@ -1,11 +1,29 @@
 import Controller from '@components/controller';
+import MyCustomer from '@services/customer-service/myCustomer';
 import type Router from '@src/app/router/router';
 
 import ProfileView from './user-profile-view/user-profile-view';
 
 class ProfileController extends Controller<ProfileView> {
+  private currentAddress = MyCustomer.addresses.addresses[0].id;
+
   constructor(private router: Router) {
-    super(new ProfileView(router));
+    super(
+      new ProfileView(
+        router,
+        (id) => this.deleteAddress(id),
+        (id) => this.editAddress(id)
+      )
+    );
+    this.getView.openAddress(this.currentAddress ?? '');
+  }
+
+  private deleteAddress(id: string) {
+    console.log('delete', id);
+  }
+
+  private editAddress(id: string) {
+    console.log('edit', id);
   }
 }
 
