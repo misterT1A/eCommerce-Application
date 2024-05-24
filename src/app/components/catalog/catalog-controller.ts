5abe50d17815275662217d5ddfbf3cfdd8c38547
Artem Taraskin <127009838+misterT1A@users.noreply.github.com>
2024-05-24 09:05:41 +0300

    feat: RSS-ECOMM-3_01 Fetch and display poduct list in Catalog (#148)
    * feat: added basic structure to the service for receiving products
    
    * feat: added method for getting product by name
    
    * feat: added product container component and card component
    
    * feat: card component updated, loader utility added, responsive design added
    
    * fix: delete buy button
    
    * fix: fixed request for receiving all products
    
    * fix: fixed input data to the product change method

--- src/app/components/catalog/catalog-controller.ts
+++ src/app/components/catalog/catalog-controller.ts
@@ -1,4 +1,7 @@
+import type { ProductProjection } from '@commercetools/platform-sdk';
+
 import Controller from '@components/controller';
+import ProductsService from '@services/product_service/product_service';
 import type Router from '@src/app/router/router';
 
 import CatalogView from './catalog-view';
@@ -6,5 +9,18 @@ import CatalogView from './catalog-view';
 export default class CatalogController extends Controller<CatalogView> {
   constructor(router: Router) {
     super(new CatalogView(router));
+
+    this.initContent();
+  }
+
+  private initContent() {
+    ProductsService.getAllProduct().then((data) => this.changeProducts(data));
+  }
+
+  // the ResProducts type can be supplemented with your own responsive option
+  private changeProducts(res: ResProducts) {
+    const products: ProductProjection[] = res.body.results;
+    // pass only the array of products
+    this.view.getProductCardView.setProducts(products);
   }
 }
