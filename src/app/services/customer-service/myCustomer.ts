0a3e20cb2553ac2e70bf377c93b5b26d8c283304
Alv0425 <vilejshikova@gmail.com>
2024-05-25 01:39:17 +0300

    feat: update myCustomer store with methods to extract customer info

--- src/app/services/customer-service/myCustomer.ts
+++ src/app/services/customer-service/myCustomer.ts
@@ -14,7 +14,7 @@ class MyCustomer {
   }
 
   public static get id() {
-    return this.info?.id;
+    return this.info?.id?.split('-')[0] ?? '';
   }
 
   public static get firstName() {
@@ -29,11 +29,62 @@ class MyCustomer {
     return `${this.firstName ?? ''} ${this.lastName ?? ''}`;
   }
 
+  public static get inn() {
+    if (!this.firstName || !this.lastName) {
+      return '';
+    }
+    return `${this.firstName[0].toUpperCase()}${this.lastName[0].toUpperCase()}`;
+  }
+
   public static get fullNameShort() {
     if (!this.firstName || !this.lastName) {
       return '';
     }
     return `${this.firstName[0].toUpperCase()}. ${this.lastName.toUpperCase()}`;
+  }
+
+  public static get email() {
+    return this.info?.email ?? '';
+  }
+
+  public static get password() {
+    const password = this.info?.password;
+    if (!password) {
+      return '';
+    }
+    return '*'.repeat(password.length);
+  }
+
+  public static get dateOfBirth() {
+    const date = this.info?.dateOfBirth;
+    if (!date) {
+      return '';
+    }
+    const dateObj = new Date(date);
+    return `${dateObj.getDay()}/${dateObj.getMonth()}/${dateObj.getFullYear()}`;
+  }
+
+  public static get addresses() {
+    return {
+      addresses: this.info?.addresses ?? [],
+      billingAddressIds: this.info?.billingAddressIds ?? [],
+      shippingAddressIds: this.info?.shippingAddressIds ?? [],
+      defaultShippingAddress: this.info?.defaultShippingAddressId,
+      defaultBillingAddress: this.info?.defaultBillingAddressId,
+    };
+  }
+
+  public static getAddressById(id: string) {
+    const { addresses } = this.addresses;
+    return addresses.find((address) => address.id === id);
+  }
+
+  public static get defaultShippingId() {
+    return this.addresses.defaultShippingAddress;
+  }
+
+  public static get defaultBillingId() {
+    return this.addresses.defaultBillingAddress;
   }
 }
 
