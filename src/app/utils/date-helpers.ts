4bed8aa0a69f612325a2a3c869dc29a69410a5d7
Alv0425 <vilejshikova@gmail.com>
2024-05-27 20:46:47 +0300

    feat: add date helper functions

---
+++ src/app/utils/date-helpers.ts
@@ -0,0 +1,3 @@
+export default function getUTCDate(date: Date) {
+  return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+}
