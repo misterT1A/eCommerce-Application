1ea80f5e68cc2df26d9e4afced0f706218481e40
Alv0425 <vilejshikova@gmail.com>
2024-05-28 22:25:23 +0300

    feat: update profile component types

--- src/types/profileTypes.d.ts
+++ src/types/profileTypes.d.ts
@@ -8,3 +8,25 @@ enum UserFormFields {
 type userFormFieldsType = keyof typeof UserFormFields;
 
 type IUserInfoValues = Record<userFormFieldsType, string>;
+
+enum ProfileAddressesFields {
+  country,
+  zipCode,
+  street,
+  city,
+  isBilling,
+  isShipping,
+  isDefault,
+}
+
+type ProfileAddressesFieldsType = keyof typeof ProfileAddressesFields;
+
+type ProfileAddressValues = {
+  country: 'UK' | 'France' | 'Belgium';
+  zipCode: string;
+  street: string;
+  city: string;
+  isBilling: boolean;
+  isShipping: boolean;
+  isDefault: boolean;
+};
