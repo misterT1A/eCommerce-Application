43b25463aa162aa47fc7c2b6e763ac82823bd9a7
Alv0425 <vilejshikova@gmail.com>
2024-05-28 22:28:48 +0300

    feat: rename date helper

--- src/app/utils/date-helpers.ts
+++ src/app/utils/date-helpers.ts
@@ -1,3 +1,3 @@
-export default function getUTCDate(date: Date) {
+export default function getISODate(date: Date) {
   return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
 }
