9b0375323b978030059b95d391f5738226a56494
Alv0425 <vilejshikova@gmail.com>
2024-05-30 01:44:09 +0300

    feat: refactor and update profile controller with logout method

--- src/app/components/user-profile/user-profile-controller.ts
+++ src/app/components/user-profile/user-profile-controller.ts
@@ -1,17 +1,17 @@
 import Controller from '@components/controller';
 import type HeaderController from '@components/header/header_controller';
-import MyCustomer from '@services/customer-service/myCustomer';
+import AuthService from '@services/auth-service';
 import type Router from '@src/app/router/router';
 
 import AddAddress from './addresses-add-mode';
 import EditAddress from './addresses-edit-mode';
+import ChangePasswordMode from './change-password-mode';
+import DeleteAccount from './delete-account-mode';
 import DeleteAddress from './delete-address-mode';
 import EditModeProfile from './profile-details-edit-mode';
 import ProfileView from './user-profile-view/user-profile-view';
 
 class ProfileController extends Controller<ProfileView> {
-  private currentAddress = MyCustomer.addresses.addresses[0].id;
-
   private profileInfoEdit: EditModeProfile;
 
   private addAddress: AddAddress;
@@ -20,6 +20,10 @@ class ProfileController extends Controller<ProfileView> {
 
   private deleteAddressMode: DeleteAddress;
 
+  private changePasswordMode: ChangePasswordMode;
+
+  private deleteAccountMode: DeleteAccount;
+
   constructor(
     private router: Router,
     private headerController: HeaderController
@@ -27,51 +31,30 @@ class ProfileController extends Controller<ProfileView> {
     super(
       new ProfileView(
         router,
-        (id) => this.deleteAddress(id),
-        (id) => this.editAddress(id),
-        async (id) => {
-          await this.setAsDefaultShipping(id);
-        },
-        async (id) => {
-          await this.setAsDefaultBilling(id);
-        }
+        (id) => this.deleteAddressMode.enable(id),
+        (id) => this.editAddressMode.enable(id),
+        (id) => this.editAddressMode.setAddressAsDefault('Shipping', id),
+        (id) => this.editAddressMode.setAddressAsDefault('Billing', id)
       )
     );
-    this.getView.openAddress(this.currentAddress ?? '');
-    this.getView.profileCredentials.editButton.addListener('click', () => {
-      this.enableUserInfoEdit();
-    });
-    this.getView.profileAddresses.addAddressButton.addListener('click', () => {
-      this.enableAddressesAdd();
-    });
+    this.getView.openFirstAddress();
+    this.getView.profileCredentials.editButton.addListener('click', () => this.profileInfoEdit.enable());
+    this.getView.profileAddresses.addAddressButton.addListener('click', () => this.addAddress.enable());
+    this.getView.changePasswordButton.addListener('click', () => this.changePasswordMode.enable());
+    this.getView.deleteAccount.addListener('click', () => this.deleteAccountMode.enable(() => this.logout()));
+    this.getView.logOutButton.addListener('click', () => this.logout());
     this.profileInfoEdit = new EditModeProfile(this.getView, this.headerController);
     this.addAddress = new AddAddress(this.getView);
     this.editAddressMode = new EditAddress(this.getView);
     this.deleteAddressMode = new DeleteAddress(this.getView);
+    this.changePasswordMode = new ChangePasswordMode(this.getView);
+    this.deleteAccountMode = new DeleteAccount(this.getView);
   }
 
-  private deleteAddress(id: string) {
-    this.deleteAddressMode.enable(id);
-  }
-
-  private editAddress(id: string) {
-    this.editAddressMode.enable(id);
-  }
-
-  private enableUserInfoEdit() {
-    this.profileInfoEdit.enable();
-  }
-
-  private enableAddressesAdd() {
-    this.addAddress.enable();
-  }
-
-  public async setAsDefaultShipping(id: string) {
-    await this.editAddressMode.setAddressAsDefault('Shipping', id);
-  }
-
-  public async setAsDefaultBilling(id: string) {
-    await this.editAddressMode.setAddressAsDefault('Billing', id);
+  private async logout() {
+    await AuthService.logout();
+    this.headerController.getView.changeTextNotLoginned();
+    this.router.navigateToLastPoint();
   }
 }
 
