6995d26b9375013802de5a863ecf38106784b00f
Alv0425 <vilejshikova@gmail.com>
2024-05-30 01:39:22 +0300

    feat: add logout, change password and delete account buttons on profile page

--- src/app/components/user-profile/user-profile-view/user-profile-view.ts
+++ src/app/components/user-profile/user-profile-view/user-profile-view.ts
@@ -1,6 +1,6 @@
 import type Router from '@src/app/router/router';
 import BaseComponent from '@utils/base-component';
-import { h2 } from '@utils/elements';
+import { button, div, h2 } from '@utils/elements';
 
 import styles from './_user-profile.scss';
 import CredentialsView from './credentials-view';
@@ -10,6 +10,12 @@ class ProfileView extends BaseComponent<HTMLElement> {
   public profileCredentials: CredentialsView;
 
   public profileAddresses: ProfileAddressesView;
+
+  public changePasswordButton: BaseComponent<HTMLButtonElement>;
+
+  public logOutButton: BaseComponent<HTMLButtonElement>;
+
+  public deleteAccount: BaseComponent<HTMLButtonElement>;
 
   constructor(
     private router: Router,
@@ -28,6 +34,10 @@ class ProfileView extends BaseComponent<HTMLElement> {
       setAsDefaultBilling
     );
     this.appendChildren([this.profileAddresses, this.profileCredentials]);
+    this.changePasswordButton = button([styles.profile__button], 'CHANGE PASSWORD');
+    this.logOutButton = button([styles.profile__button], 'LOG OUT');
+    this.deleteAccount = button([styles.profile__button], 'DELETE ACCOUNT');
+    this.append(div([styles.profile__footer], this.changePasswordButton, this.logOutButton, this.deleteAccount));
   }
 
   public openAddress(id: string) {
