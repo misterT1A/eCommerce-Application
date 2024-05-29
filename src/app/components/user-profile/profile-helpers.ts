865ab2b1898b110aa15ccffd24d82e238b8da1a6
Alv0425 <vilejshikova@gmail.com>
2024-05-29 15:36:37 +0300

    feat: add helper function for address form field validation

--- src/app/components/user-profile/profile-helpers.ts
+++ src/app/components/user-profile/profile-helpers.ts
@@ -1,8 +1,11 @@
+import FormField from '@components/form-ui-elements/formField';
 import MyCustomer from '@services/customer-service/myCustomer';
+import RegistrationValidator from '@services/registrationValidationService/registrationValidator';
 import type BaseComponent from '@utils/base-component';
 import { span } from '@utils/elements';
 
 import styles from './user-profile-view/_user-profile.scss';
+import type UserAddressEdit from './user-profile-view/edit-mode/edit-address';
 
 export default function generateTags(addressID: string) {
   const markers = {
@@ -25,4 +28,21 @@ export default function generateTags(addressID: string) {
     tags.push(span([styles.profile__addressTag], `shipping default`));
   }
   return tags;
+}
+
+export function processAddressData(form: UserAddressEdit) {
+  let isValidForm = true;
+  const errorsObject = RegistrationValidator.processAddressInfo(form.getValues());
+  Object.entries(errorsObject).forEach(([key, errors]) => {
+    if (errors.length > 0) {
+      isValidForm = false;
+    }
+    const field = form.fields[key as ProfileAddressesFieldsType];
+    if (field instanceof FormField) {
+      field.updateErrors(errors);
+    }
+  });
+  return {
+    isValidForm,
+  };
 }
