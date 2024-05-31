42d389c2154283787ce7240a42d7d21b6e857eda
tatsianask108 <tt.laurie@gmail.com>
2024-05-31 15:54:07 +0300

    fix: add resize handler

--- src/app/components/catalog/filters/toggle-filters.ts
+++ src/app/components/catalog/filters/toggle-filters.ts
@@ -11,7 +11,21 @@ export default class ToggleFiltersBlock extends BaseComponent {
       this.filtersBlock.addClass(styles['filterBlock--visible']);
       document.body.style.overflow = 'hidden';
     });
+    window.addEventListener('resize', () => this.resizeHandler());
 
     this.appendChildren([button([styles['toggle-filters-btn']], ''), span([styles.span], 'Filters')]);
+  }
+
+  private resizeHandler() {
+    const width = window.innerWidth;
+    const isFiltersBlockVisible = this.filtersBlock.getNode().classList.contains(styles['filterBlock--visible']);
+
+    if (isFiltersBlockVisible) {
+      if (width > 1100) {
+        document.body.style.overflow = '';
+      } else {
+        document.body.style.overflow = 'hidden';
+      }
+    }
   }
 }
