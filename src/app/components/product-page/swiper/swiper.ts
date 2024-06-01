70e35a442415349ded182db257144a036374a8c3
tatsianask108 <tt.laurie@gmail.com>
2024-06-01 22:40:13 +0300

    feat: implement modal with enlarged images slider

--- src/app/components/product-page/swiper/swiper.ts
+++ src/app/components/product-page/swiper/swiper.ts
@@ -1,30 +1,47 @@
-// import Swiper bundle with all modules installed
 import Swiper from 'swiper/bundle';
 
-// import styles bundle
-import 'swiper/css/bundle';
-
 // init Swiper:
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
+const initSwiper = () =>
+  new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: 'fade',
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
+const initZoomedSwiper = () =>
+  new Swiper('.zoomedSwiper', {
+    direction: 'horizontal',
+    zoom: true,
+    effect: 'fade',
+    loop: true,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
 
-export default swiper;
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+export { initSwiper, initZoomedSwiper };
