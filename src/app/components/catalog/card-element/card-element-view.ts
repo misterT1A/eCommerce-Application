ece19d65754c8580f6c575d086cf2aabf931d8dd
Alv0425 <vilejshikova@gmail.com>
2024-06-09 23:45:00 +0300

    feat: update card element view with controls required for the adding/deletion of products in cart

--- src/app/components/catalog/card-element/card-element-view.ts
+++ src/app/components/catalog/card-element/card-element-view.ts
@@ -1,14 +1,20 @@
 import type { Price } from '@commercetools/platform-sdk';
 
+import AddToCart from '@components/form-ui-elements/addToCartToggler';
+import Count from '@components/form-ui-elements/countInput';
 import type Router from '@src/app/router/router';
 import BaseComponent from '@utils/base-component';
 import { button, div, h2, p } from '@utils/elements';
 import setLoader from '@utils/loader/loader-view';
 
 import styles from './_card-style.scss';
-import { changeCount, setPrice, setShortDescription } from './card-model';
+import { setPrice, setShortDescription } from './card-model';
 
 export default class Card extends BaseComponent {
+  private addBtn = new AddToCart();
+
+  private count = new Count();
+
   constructor(
     protected props: ICardProps,
     protected router: Router
@@ -24,8 +30,12 @@ export default class Card extends BaseComponent {
       ...this.cteateTextandBtn(props),
       this.createPayBlock(props),
     ]);
-
     this.addListener('click', (e: Event) => this.handler(e));
+    this.setInputHandlers();
+    this.count.setValue(props.count || 1);
+    if (this.props.isSelected) {
+      this.addBtn.select();
+    }
   }
 
   private createImg(url: string): BaseComponent {
@@ -52,6 +62,8 @@ export default class Card extends BaseComponent {
     const title = h2([styles.title], props.title);
     const description = p([styles.description], setShortDescription(props.description));
     const readMoreBtn = button([styles.read_more_btn], 'READ MORE');
+    readMoreBtn.getNode().setAttribute('data-name', 'read-more-button');
+
     return [title, description, readMoreBtn];
   }
 
@@ -67,14 +79,26 @@ export default class Card extends BaseComponent {
       price.addClass(styles.price);
     }
 
-    const countMinus = p([styles.count_minus], '-');
-    const countPlus = p([styles.count_plus], '+');
-    const countNum = p([styles.count_Num], '1');
-    const countWrapper = div([styles.count_wrapper], countMinus, countNum, countPlus);
+    return div([styles.pay_block], price, div([styles.pay_block_controls], this.count, this.addBtn));
+  }
 
-    // const buyBtn = button([styles.buy_btn], 'Buy');
-
-    return div([styles.pay_block], price, countWrapper);
+  private setInputHandlers() {
+    this.addListener('input', (e) => {
+      const isAddInput = (e.target as HTMLInputElement)?.dataset.name === 'add-to-cart-input';
+      if (!this.count.getValue() || this.count.getValue() <= 0) {
+        if (this.addBtn.getValue()) {
+          this.addBtn.unselect();
+          // TODO: remove elected product from cart
+        }
+        this.count.setValue(1);
+      } else if (this.addBtn.getValue()) {
+        if (isAddInput) {
+          // TODO: add to cart with specified count
+        } else {
+          // TODO: change count of selected product in cart
+        }
+      }
+    });
   }
 
   public setAnimDelay(index: number) {
@@ -83,21 +107,18 @@ export default class Card extends BaseComponent {
   }
 
   private handler(e: Event) {
-    const target = (e.target as HTMLElement)?.textContent;
+    const target = (e.target as HTMLInputElement)?.dataset.name;
     switch (target) {
-      case '+':
-        changeCount((e.target as HTMLElement).parentElement, true);
-        break;
-      case '-':
-        changeCount((e.target as HTMLElement).parentElement, false);
-        break;
-      case 'READ MORE':
+      case 'read-more-button':
         this.router.savePath();
         this.router.navigateToProduct(this.props.key);
         break;
-      // case 'Buy':
-      // this.router.navigate(Pages.);
-      // break;
+      // case 'add-to-cart':
+      //   if (this.addBtn.getValue()) {
+      //     this.router.savePath();
+      //     this.router.navigate(Pages.CART);
+      //   }
+      //   break;
       default:
         break;
     }
